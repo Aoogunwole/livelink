979c321c2050331acb76c16b3cbe3dc701f1bf0a
Abraham Ogunwole <ogunwoleabraham21@gmail.com>
2025-01-22 09:23:04 +0100

    video setup

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+export const sidebarLinks = [
+    {
+      
+      label: 'Home',
+      route: '/',
+      imgURL: '/icons/Home.svg',
+    },
+  
+    {
+      label: 'Upcoming',
+      route: '/upcoming',
+      imgURL: '/icons/upcoming.svg',
+    },
+    {
+      imgURL: '/icons/previous.svg',
+      route: '/previous',
+      label: 'Previous',
+    },
+    {
+      imgURL: '/icons/Video.svg',
+      route: '/recordings',
+      label: 'Recordings',
+    },
+    {
+      imgURL: '/icons/add-personal.svg',
+      route: '/personal-room',
+      label: 'Personal Room',
+    },
+  ];
